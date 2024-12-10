d7bcf6ffb3e8f7fab9dec91cf853ce57bb6f6a4c
ezin1 <eziof.dev@gmail.com>
2024-12-10 16:35:22 -0300

    feat: insert button component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,7 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import { Welcome } from "@/components/welcome";
 import { Steps } from "@/components/steps";
+import { Button} from "@/components/button";
 
 
 export default function Index() {
@@ -14,6 +15,9 @@ export default function Index() {
     >
       <Welcome />
       <Steps />
+      <Button isLoading={true}>
+        <Button.Title>Começar</Button.Title>
+      </Button>
     </View>
   );
 }
